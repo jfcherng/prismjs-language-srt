e84c58bf937c0d67998166e8824632f4f4fa45bf
Jack Cherng <jfcherng@gmail.com>
2018-03-12 01:46:30 +0800

    init

---
+++ prism.language.srt.js
@@ -0,0 +1,35 @@
+/*!
+ * SRT subtitle syntax highlight for Prism.js
+ *
+ * @author Jack Cherng <jfcherng@gmail.com>
+ * @version 1.0.0
+ * @license MIT
+ */
+Prism.languages.srt = {
+    'id': {
+        pattern: /^\d+$/m,
+        alias: 'number'
+    },
+    'time-line': {
+        pattern: /(^|\s)(\d+\s*[:：]\s*\d+\s*[:：]\s*\d+[,，.．]\d+)\s*-+>\s*(\d+\s*[:：]\s*\d+\s*[:：]\s*\d+[,，.．]\d+)/m,
+        lookbehind: true,
+        inside: {
+            'time': {
+                pattern: /\d+\s*[:：]\s*\d+\s*[:：]\s*\d+[,，.．]\d+/m,
+                alias: 'string'
+            },
+            'dashed-arrow': {
+                pattern: /-+>/,
+                alias: 'punctuation'
+            },
+            'space': / /
+        }
+    },
+    'ass-tag': {
+        pattern: /\{@*\\[^}\r\n]+\}/,
+        inside: {
+            'space': / /
+        },
+        alias: 'comment'
+    }
+};
